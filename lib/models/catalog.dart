06aa817b15059dcbf4a41870f039627134d6f164
TechBrahama <agupta20comp@student.mes.ac.in>
2021-12-15 20:18:12 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,29 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iphone 12 pro",
+        desc: "Apple phone",
+        price: 777,
+        color: "#33505a",
+        image:
+            "https://images.pexels.com/photos/8066715/pexels-photo-8066715.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       // it is a canavas which haves child
       appBar: AppBar(
@@ -16,11 +19,19 @@ class HomePage extends StatelessWidget {
         //backgroundColor: Colors.white,
         // iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to my First Flutter App!!!!! "),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return Itemwidget(
+              //item: CatalogModel.items[index],
+              item: dummylist[index],
+            );
+          },
         ),
       ),
+
       drawer: MyDrawer(),
     );
   }
