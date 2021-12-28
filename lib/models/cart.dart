6c7170f362dac2b11e167dd0b562fae2b5c9efc7
TechBrahama <agupta20comp@student.mes.ac.in>
2021-12-28 12:23:47 +0530

    day 27 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,8 +1,8 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-
-
   late CatalogModel _catalog;
 
   final List<int> _itemIds = []; //stores id of each element
@@ -21,14 +21,18 @@ class CartModel {
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
 
-  // to add item
-
-  void add(Item item) {
-    _itemIds.add(item.id);
-  }
-
   // remove item
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutattion extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutattion(this.item);
+  @override
+  perform() {
+    store!.cart._itemIds.add(item.id);
   }
 }
