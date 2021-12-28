fba27cbc4969ae4e730aa0458341205a8fa40547
TechBrahama <agupta20comp@student.mes.ac.in>
2021-12-28 11:49:15 +0530

    day 26 completed

--- lib/pages/home_widgets/catalog_list.dart
+++ lib/pages/home_widgets/catalog_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/pages/home_details_page.dart';
 import 'package:velocity_x/velocity_x.dart';
